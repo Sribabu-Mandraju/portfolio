df7aec266fe5be24fdcb102c89740555a6919075
sribabu <sribabumandraju@gmail.com>
2025-05-12 09:56:11 +0530

    new

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -24,7 +24,6 @@ const DashboardLayout = ({ children }) => {
       <div className="flex-1 flex flex-col min-h-screen">
         {/* Navbar */}
         <Navbar onMenuClick={() => setIsSidebarOpen(true)} />
-
         {/* Main Content Area */}
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 dark:bg-gray-900">
           <div className="container mx-auto px-6 py-8">
